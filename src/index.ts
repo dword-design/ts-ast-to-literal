1842efc6f23692c11d5d0256a7240c42e7b89da4
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-12 04:22:22 +0200

    fix: upgrades

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import ts from 'typescript';
+
+const traverse = <T = unknown>(node: ts.Node): T => {
+  switch (node.kind) {
+    case ts.SyntaxKind.StringLiteral: {
+      const stringLiteral = node as ts.StringLiteral;
+      return stringLiteral.text as T;
+    }
+
+    case ts.SyntaxKind.TrueKeyword: {
+      return true as T;
+    }
+
+    case ts.SyntaxKind.FalseKeyword: {
+      return false as T;
+    }
+
+    case ts.SyntaxKind.NullKeyword: {
+      return null as T;
+    }
+
+    case ts.SyntaxKind.NumericLiteral: {
+      const numericLiteral = node as ts.NumericLiteral;
+      return Number.parseFloat(numericLiteral.text) as T;
+    }
+
+    case ts.SyntaxKind.ArrayLiteralExpression: {
+      const arrayLiteral = node as ts.ArrayLiteralExpression;
+      return arrayLiteral.elements
+        .filter(element => element.kind !== ts.SyntaxKind.SpreadElement)
+        .map(element => traverse(element)) as T;
+    }
+
+    case ts.SyntaxKind.ObjectLiteralExpression: {
+      const objectLiteral = node as ts.ObjectLiteralExpression;
+      return Object.fromEntries(
+        objectLiteral.properties
+          .filter(
+            property =>
+              property.kind === ts.SyntaxKind.PropertyAssignment &&
+              (property.name.kind === ts.SyntaxKind.Identifier ||
+                property.name.kind === ts.SyntaxKind.StringLiteral),
+          )
+          .map(property => {
+            const propAssignment = property as ts.PropertyAssignment;
+            const name = propAssignment.name as ts.Identifier | ts.StringLiteral;
+            return [
+              (name as any).escapedText || (name as any).text,
+              traverse(propAssignment.initializer),
+            ];
+          }),
+      ) as T;
+    }
+
+    default: {
+      return undefined as T;
+    }
+  }
+};
+
+export default traverse;
